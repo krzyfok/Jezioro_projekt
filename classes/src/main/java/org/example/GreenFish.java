138bcdf156bb3b1a6a9d67ad86bd5f5107785c71
glob9910 <115173550+glob9910@users.noreply.github.com>
2023-05-17 08:59:23 +0200

    Zmiana na pojedyncza arrayliste 4

--- classes/src/main/java/org/example/GreenFish.java
+++ classes/src/main/java/org/example/GreenFish.java
@@ -1,18 +1,9 @@
 package org.example;
+
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Random;
 public class GreenFish extends Fish{
-    static ArrayList<GreenFish> tablicaRyb;       //przechowuje wszystkie rybki tej odmiany
 
-
-    GreenFish(int fishnumber) {
-        tablicaRyb = new ArrayList();               //tutaj utworzenie rybek w ilosci zadanej w mainie
-
-        for(int i=0; i<fishnumber; i++)
-            tablicaRyb.add(new GreenFish());  //odwolanie do konstruktora "pojedynczej ryby", ktora jest nizej
-
-    }
 
     GreenFish() {  //"konstruktor pojedynczej ryby"
         Random rand = new Random();
@@ -24,45 +15,47 @@ public class GreenFish extends Fish{
         this.power=10;
         this.agility=10;
         this.position(size);
-
-
     }
+
     public void swim(Graphics g) {
+
+
         g.setColor(Color.GREEN);
-        for (GreenFish fish : tablicaRyb) {
-            g.fillOval(fish.coX, fish.coY, fish.size, fish.size);
-            if(fish.coX >= Map.border && fish.coX <= Map.size- Map.border-size) {
-                if(fish.gobackx==true)
-                    fish.coX += fish.speed;
+
+            g.fillOval(this.coX, this.coY, this.size, this.size);
+            if(this.coX >= Map.border && this.coX <= Map.size- Map.border-size) {
+                if(this.gobackx==true)
+                    this.coX += this.speed;
                 else
-                    fish.coX -= fish.speed;
+                    this.coX -= this.speed;
             }
             else {                                              //tutaj ryba zawraca gdy spotka krawedz
-                if(fish.gobackx==true) {
-                    fish.gobackx=false;
-                    fish.coX -= fish.speed;
+                if(this.gobackx==true) {
+                    this.gobackx=false;
+                    this.coX -= this.speed;
                 }
                 else {
-                    fish.gobackx=true;
-                    fish.coX += fish.speed;
+                    this.gobackx=true;
+                    this.coX += this.speed;
                 }
             }
-            if(fish.coY >= Map.upBorder && fish.coY <= Map.size- Map.downBorder-size) {
-                if(fish.gobacky==true)
-                    fish.coY += fish.speed/2;
+            if(this.coY >= Map.upBorder && this.coY <= Map.size- Map.downBorder-size) {
+                if(this.gobacky==true)
+                    this.coY += this.speed/2;
                 else
-                    fish.coY -= fish.speed/2;
+                    this.coY -= this.speed/2;
             }
             else {                                              //tutaj ryba zawraca gdy spotka krawedz
-                if(fish.gobacky==true) {
-                    fish.gobacky=false;
-                    fish.coY -= fish.speed/2;
+                if(this.gobacky==true) {
+                    this.gobacky=false;
+                    this.coY -= this.speed/2;
                 }
                 else {
-                    fish.gobacky=true;
-                    fish.coY += fish.speed/2;
+                    this.gobacky=true;
+                    this.coY += this.speed/2;
                 }
             }
-        }
+
+
     }
 }
