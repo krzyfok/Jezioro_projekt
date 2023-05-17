fc4c61abb2a7c71ea0e176f4d4d56a7c72ec408c
glob9910 <115173550+glob9910@users.noreply.github.com>
2023-05-17 08:58:01 +0200

    Zmiana na pojedyncza arrayliste 2

--- classes/src/main/java/org/example/Fish.java
+++ classes/src/main/java/org/example/Fish.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import java.awt.*;
 import java.util.Random;
 
@@ -12,24 +13,17 @@ public abstract class Fish {
     protected int power;
     protected int agility;
     protected int health;
-    protected boolean gobackx;           //jeszcze nie wiem jak ogarnac jak to zrobic zeby ryby sie odbijaly od sciany, wiec na probe zrobilam im zmienna boolowska, ktora po uderzeniu w sciane sie zmienia i zmieniaja kierunek w metodzie plywania
+    protected boolean gobackx;
     protected boolean gobacky;
     public  void position(int size)
     {
         //tu na dole ustawiana jest pierwsza lokalizacja ryby, z uwzglednieniem warunku, zeby byla na planszy
         Random rand = new Random();
-       // do {
-       //     this.coX = rand.nextInt()%(Draw.size-Draw.border);
-      //  }while(coX <= Draw.border || coX >= Draw.size-2*Draw.border);
+
         this.coX=((rand.nextInt(Map.size-2* Map.border-size))+ Map.border);
-       // do {
-       //     this.coY = rand.nextInt()%(Draw.size-Draw.border);
-      //  }while(coY <= Draw.upBorder || coY >= Draw.size-2*Draw.border);
         this.coY=((rand.nextInt(Map.size- Map.upBorder- Map.downBorder-size))+ Map.upBorder);
 
     }
-    //public static void swim(Graphics g){        //tu musi byc to g do rysowania. W tej metodzie od razu rysuje te ryby(na razie kulki), mozna to podzielic na wiecej metod. Np oddzielna do rysowania i oddzielna do zmieniania wspolrzednych
+    public void swim(Graphics g){}
 
-
-  //  }
 }
