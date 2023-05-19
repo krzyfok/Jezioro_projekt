95747d6c8f5a809cb08d7184d8020b88ea075dae
glob9910 <115173550+glob9910@users.noreply.github.com>
2023-05-19 19:49:11 +0200

    atak1

--- classes/src/main/java/org/example/Fish.java
+++ classes/src/main/java/org/example/Fish.java
@@ -17,6 +17,8 @@ public abstract class Fish {
     protected boolean gobackx;
     protected boolean gobacky;
     protected static int breedrange = 10;       //mozna dodac inny range rozmnazania dla roznych ryb
+    protected static int attackrange = 50;         // -//-
+    protected int hunger;
     protected int dobreed;                  //czy moga sie rozmnazac
     public  void position(int size)
     {
@@ -31,51 +33,57 @@ public abstract class Fish {
     public static double distance(Fish fish1, Fish fish2) {
         return (Math.sqrt((fish1.coX-fish2.coX)*(fish1.coX-fish2.coX)+(fish1.coY-fish2.coY)*(fish1.coY-fish2.coY)));
     }
-    public void reproduct() {
-
+    public void reproduct(Fish fish2) {
+        Random rand = new Random();
 
         //"indeks" dodawanej rybki. Przydaje sie do ustawienia jej wspolrzednych
 
-        for(int i=0; i<Map.tableOfFish.size(); i++) {
-            for(int j=0; j<Map.tableOfFish.size(); j++) {
-
-                //jesli sa te same lub nie moga jeszcze sie rozmnazac to skip
-                if(i==j)
-                    continue;
-                if(Map.tableOfFish.get(i).dobreed<=50 || Map.tableOfFish.get(j).dobreed<=50)
-                    continue;
 
 
-
-                //sprawdzenie czy dystans jest mniejszy niz range i odmiany rybek sa te same:
-                if (distance(Map.tableOfFish.get(i), Map.tableOfFish.get(j)) <= breedrange) {
-                    if (Map.tableOfFish.get(i).getClass() == Map.tableOfFish.get(j).getClass()) {
-
-                        if (Map.tableOfFish.get(i) instanceof GreenFish)
-                            Map.tableOfFish.add(new GreenFish());
-                        else if (Map.tableOfFish.get(i) instanceof RedFish)
-                            Map.tableOfFish.add(new RedFish());
-                        else if (Map.tableOfFish.get(i) instanceof YellowFish)
-                            Map.tableOfFish.add(new YellowFish());
+        if(this.dobreed<=50 || fish2.dobreed<=50)
+            return;
 
 
+        //sprawdzenie czy dystans jest mniejszy niz range i odmiany rybek sa te same:
+        if (distance(this, fish2) <= breedrange) {
 
-                        //ustawienie wspolrzednych dziecka ryb:
-                     //   Map.tableOfFish.get(Map.tableOfFish.size()-1).coX = 500;
-                       // Map.tableOfFish.get(Map.tableOfFish.size()-1).coY = 500;
-                        Map.tableOfFish.get(Map.tableOfFish.size()-1).position(this.size);
-                        Map.tableOfFish.get(Map.tableOfFish.size()-1).gobackx = !Map.tableOfFish.get(i).gobackx;     //zawraca? Zeby nie plynely razem bo beda sie rozmnazac ze soba
-                        Map.tableOfFish.get(Map.tableOfFish.size()-1).gobacky = !Map.tableOfFish.get(i).gobacky;
+            if (this instanceof GreenFish)
+                Map.tableOfFish.add(new GreenFish());
+            else if (this instanceof RedFish)
+                Map.tableOfFish.add(new RedFish());
+            else if (this instanceof YellowFish)
+                Map.tableOfFish.add(new YellowFish());
 
-                        Map.tableOfFish.get(i).dobreed = 0;           //te rybki nie beda mogly juz sie rozmnazac przez jakis czas
-                        Map.tableOfFish.get(j).dobreed = 0;
 
-                    }
-                }
-            }
+            //ustawienie wspolrzednych dziecka ryb:
+            //Map.tableOfFish.get(Map.tableOfFish.size()-1).coX = this.coX-rand.nextInt()%15;;
+            //Map.tableOfFish.get(Map.tableOfFish.size()-1).coY = this.coY-rand.nextInt()%15;
+            Map.tableOfFish.get(Map.tableOfFish.size()-1).position(this.size);                             //generuje sie losowo na mapie
+            Map.tableOfFish.get(Map.tableOfFish.size()-1).gobackx = !this.gobackx;     //zawraca? Zeby nie plynely razem bo beda sie rozmnazac ze soba
+            Map.tableOfFish.get(Map.tableOfFish.size()-1).gobacky = !this.gobacky;
+
+            this.dobreed = 0;           //te rybki nie beda mogly juz sie rozmnazac przez jakis czas
+            fish2.dobreed = 0;
         }
-
 
     }
 
+    public void attack(Fish fish2) {
+        if (distance(this, fish2) <= breedrange) {      //tu na razie jest breedrange. mozna potem ustawic jakis attackrange
+            if (this.power > fish2.power) {
+                fish2.health -= this.power / 2;     //traci jakby polowe hp, bo podczas wywolania tej metody w Map, bedzie ona wywolana 2 razy dla tego samego zestawu ryb. wiec lacznie straci 2 razy po pol
+                this.hunger=0;
+            }
+            else {
+                this.health -= fish2.power / 2;
+                fish2.hunger=0;
+            }
+        }
+    }
+
+
+    public void die() {
+        if (this.health<=0)
+            Map.tableOfFish.remove(this);
+    }
 }
