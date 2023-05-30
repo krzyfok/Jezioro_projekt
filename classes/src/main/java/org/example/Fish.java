7828efbd827406e29692a2ada59cd2ec1d841953
glob9910 <115173550+glob9910@users.noreply.github.com>
2023-05-30 13:40:37 +0000

    Update Fish.java

--- classes/src/main/java/org/example/Fish.java
+++ classes/src/main/java/org/example/Fish.java
@@ -1,9 +1,13 @@
 package org.example;
 
+
+
+
 import java.awt.*;
 import java.util.Random;
 
 public abstract class Fish {
+
 
     protected int speedX;
     protected int speedY;
@@ -16,35 +20,51 @@ public abstract class Fish {
     protected int health;
     protected boolean gobackx;
     protected boolean gobacky;
-    protected static int breedrange = 10;       //mozna dodac inny range rozmnazania dla roznych ryb
-    protected static int attackrange = 50;         // -//-
-    protected int hunger;
-    protected int dobreed;                  //czy moga sie rozmnazac
-    public  void position(int size)
-    {
+    protected static int breedrange = 20;
+    protected int attackrange;
+    protected double hunger;
+    protected double dobreed;      //czy moga sie rozmnazac
+
+
+
+    public  void position(int size) {
+
         //tu na dole ustawiana jest pierwsza lokalizacja ryby, z uwzglednieniem warunku, zeby byla na planszy
         Random rand = new Random();
+        this.coX=((rand.nextInt(Map.size-2* Map.border-2*size))+ Map.border+size);
+        this.coY=((rand.nextInt(Map.size- Map.upBorder- Map.downBorder-2*size-2*Map.border))+ Map.upBorder+2*size+Map.border);
 
-        this.coX=((rand.nextInt(Map.size-2* Map.border-size))+ Map.border);
-        this.coY=((rand.nextInt(Map.size- Map.upBorder- Map.downBorder-size))+ Map.upBorder);
 
+         //korekta polozenia ryby
+        if(this.coY<=Map.upBorder+Map.border) {
+            this.coY+=2*this.size;
+            this.gobacky=true;
+        }
+        if(this.coY>=Map.size-Map.downBorder-Map.border-this.size) {
+            this.coY-=2*this.size;
+            this.gobacky=false;
+        }
     }
+
+
     public void swim(Graphics g){}
+
     public static double distance(Fish fish1, Fish fish2) {
-        return (Math.sqrt((fish1.coX-fish2.coX)*(fish1.coX-fish2.coX)+(fish1.coY-fish2.coY)*(fish1.coY-fish2.coY)));
+        return (Math.sqrt((fish1.coX+fish1.size/2-fish2.coX-fish2.size/2)*(fish1.coX+fish1.size/2-fish2.coX-fish2.size/2)+(fish1.coY+fish1.size/2-fish2.coY-fish2.size/2)*(fish1.coY+fish1.size/2-fish2.coY-fish2.size/2)));
     }
+
     public void reproduct(Fish fish2) {
         Random rand = new Random();
 
-        //"indeks" dodawanej rybki. Przydaje sie do ustawienia jej wspolrzednych
 
-
-
-        if(this.dobreed<=50 || fish2.dobreed<=50)
+        //sprawdzenie czy moga sie rozmnazac
+        if(this.dobreed<=30 || fish2.dobreed<=30)
+            return;
+        if(this.numberOfFish()>=45)      //ograniczenie do 45 osobnikow danej odmiany
             return;
 
 
-        //sprawdzenie czy dystans jest mniejszy niz range i odmiany rybek sa te same:
+        //sprawdzenie czy dystans jest mniejszy niz range:
         if (distance(this, fish2) <= breedrange) {
 
             if (this instanceof GreenFish)
@@ -55,12 +75,25 @@ public abstract class Fish {
                 Map.tableOfFish.add(new YellowFish());
 
 
-            //ustawienie wspolrzednych dziecka ryb:
-            //Map.tableOfFish.get(Map.tableOfFish.size()-1).coX = this.coX-rand.nextInt()%15;;
-            //Map.tableOfFish.get(Map.tableOfFish.size()-1).coY = this.coY-rand.nextInt()%15;
-            Map.tableOfFish.get(Map.tableOfFish.size()-1).position(this.size);                             //generuje sie losowo na mapie
-            Map.tableOfFish.get(Map.tableOfFish.size()-1).gobackx = !this.gobackx;     //zawraca? Zeby nie plynely razem bo beda sie rozmnazac ze soba
-            Map.tableOfFish.get(Map.tableOfFish.size()-1).gobacky = !this.gobacky;
+
+            //ustawienie wspolrzednych ryb dziecka
+
+            if(!(this instanceof YellowFish)) {         //zolta ryba generuje sie losowo na mapie, bo inaczej laguje
+                Map.tableOfFish.get(Map.tableOfFish.size() - 1).coX = this.coX - rand.nextInt() % 30;
+                Map.tableOfFish.get(Map.tableOfFish.size() - 1).coY = this.coY - rand.nextInt() % 30;
+
+
+                //korekta polozenia ryby
+                if(this.coY<=Map.upBorder+Map.border) {
+                    this.coY+=2*this.size;
+                    this.gobacky=true;
+                }
+                if(this.coY>=Map.size-Map.downBorder-Map.border-this.size) {
+                    this.coY-=2*this.size;
+                    this.gobacky=false;
+                }
+            }
+
 
             this.dobreed = 0;           //te rybki nie beda mogly juz sie rozmnazac przez jakis czas
             fish2.dobreed = 0;
@@ -69,24 +102,31 @@ public abstract class Fish {
     }
 
     public void attack(Fish fish2) {
-        if(this.hunger<=50 || fish2.hunger<=50)
+
+        if(this.hunger<10)
             return;
-        
-        if (distance(this, fish2) <= breedrange) {      //tu na razie jest breedrange. mozna potem ustawic jakis attackrange
+        if (distance(this, fish2) <= this.attackrange)
             if (this.power > fish2.power) {
-                fish2.health -= this.power / 2;     //traci jakby polowe hp, bo podczas wywolania tej metody w Map, bedzie ona wywolana 2 razy dla tego samego zestawu ryb. wiec lacznie straci 2 razy po pol
-                this.hunger=0;
+                fish2.health -= this.power;
+                this.hunger = 0;
             }
-            else {
-                this.health -= fish2.power / 2;
-                fish2.hunger=0;
-            }
-        }
     }
 
-
-    public void die() {
-        if (this.health<=0)
+    public int die() {
+        if (this.health<=0) {
             Map.tableOfFish.remove(this);
+            return 1;
+        }
+        else
+            return 0;
+    }
+
+    public int numberOfFish() {
+        int number=0;
+        for(int i=0; i<Map.tableOfFish.size(); i++)
+            if(Map.tableOfFish.get(i).getClass() == this.getClass())
+                number++;
+
+        return number;
     }
 }
