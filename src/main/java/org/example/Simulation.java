954e20076a8d43e978c6772a8e6c3db82f722c0d
glob9910 <115173550+glob9910@users.noreply.github.com>
2023-06-06 11:46:28 +0200

    Update Simulation.java

--- src/main/java/org/example/Simulation.java
+++ src/main/java/org/example/Simulation.java
@@ -9,18 +9,4 @@ public class Simulation {
         Map.setFish(20,10,10);
         Map symulacja = new Map();
     }
-
-
-
-//w tych klasach rybak, czerwona i zielona ryba duzo tekstu sie powtarza (min plywanie). Da sie to zrobic jakos bez takiego powtarzania?
-
-
-
-//+musi byc przynajmniej 5 klas
-//+klasy abstrakcyjne i interfejsy
-//musi uwzgledniac hermetyzacje atrybutow i metod, dziedziczenie, kompozycje/agregacje oraz polimorfizm
-
-//elementy dodatkowe:
-//gui, testy jednostkowe, repozytorium (github)
-
 }
