21062e1cd1fa838dddb9d5202a894db15789561a
glob9910 <115173550+glob9910@users.noreply.github.com>
2023-06-12 17:41:35 +0200

    Update Simulation.java

--- src/main/java/org/example/Simulation.java
+++ src/main/java/org/example/Simulation.java
@@ -7,7 +7,7 @@ public class Simulation {
     public static void main(String[] args) {
 
         Map.setFish(20,10,10,60);//parametry inicjalizacyjne, ilość poszczególnych rybek i czas trwania symulacji w sekundach
-        Map symulacja = new Map();
+        Map simulation = new Map();
 
     }
 }
